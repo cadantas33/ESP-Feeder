2b40a3c8d587492fdaee42c5033b0c94a8032184
cadantas33 <caiocd7@protonmail.com>
2023-11-01 08:53:54 -0300

    modified:   ESP_Feeder.ino 	new file:   README.md 	new file:   WebPage.txt 	modified:   index.html

--- ESP_Feeder.ino
+++ ESP_Feeder.ino
@@ -1,7 +1,5 @@
-#include <Arduino.h>
 #include <Wire.h>
 #include <Adafruit_I2CDevice.h>
-#include <Adafruit_BusIO_Register.h>
 #include <RTClib.h>
 #include <Servo.h>
 #include <Ultrasonic.h>
@@ -13,6 +11,7 @@
 Servo servo;
 RTC_DS1307 rtc;
 LiquidCrystal lcd (22, 21, 5, 18, 23, 19);
+Ultrasonic ultra(trigPin, echoPin);
 
 const int pinServo = 4;
 const int pinSensor = 17;
@@ -20,59 +19,14 @@ const int echoPin = 26;
 const int trigPin = 25;
 long distancia;
 long duracao;
+
 const char* ssid = "Rede";
 const char* password = "Senha";
 
-const char* ENTRADA_1 = "Hora 1";
-const char* ENTRADA_2 = "Minuto 1";
-const char* ENTRADA_3 = "Hora 2";
-const char* ENTRADA_4 = "Minuto 2";
-const char* ENTRADA_5 = "Hora 3";
-const char* ENTRADA_6 = "Minuto 3";
 
-Ultrasonic ultra(trigPin, echoPin);
-
-const char index_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE HTML><html><head>
-    <title>Entradad de Horários</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    </head><body>
-    <div style="display: flex;">
-      <form action="/get">
-        Hora 1: <input type="text" name="Hora 1">
-        <input type="submit" value="Enviar">
-      </form>
-      <form action="/get">
-        Minuto 1: <input type="text" name="Minuto 1">
-        <input type="submit" value="Enviar">
-      </form>
-    </div>
-    <br>
-    <div style="display: flex;">
-    <form action="/get">
-      Hora 2: <input type="text" name="Hora 2">
-      <input type="submit" value="Enviar">
-    </form>
-    <form action="/get">
-        Minuto 2: <input type="text" name="Minuto 2">
-        <input type="submit" value="Enviar">
-      </form>
-    </div>
-    <br>
-    <div style="display: flex;">
-      <form action="/get">
-        Hora 3: <input type="text" name="Hora 3">
-        <input type="submit" value="Enviar">
-      </form>
-      <form action="/get">
-        Minuto 3: <input type="text" name="Minuto 3">
-        <input type="submit" value="Enviar">
-      </form>
-    </div>
-  </body></html>)rawliteral";
-
-void ultrassonico();
+void distanceCalc();
 void infraRed();
+void levelCheck();
 
 void setup() {
   Serial.begin(115200);
@@ -94,7 +48,9 @@ void setup() {
 }
 
 void loop() {
-  ultrassonico();
+  void distanceCalc();
+  void infraRed();
+  void levelCheck();
 
   lcd.clear();
 
@@ -114,6 +70,31 @@ void loop() {
   Serial.println(now.second(), DEC);
   delay(1000);
 
+  if(now.hour() == "variavel_hora" && now.minute() == "variavel_minutos"){
+    servo.write(90);
+  }
+  
+}
+
+void distanceCalc() {
+
+  digitalWrite(trigPin, LOW); //Pulsando o sensor ultrassônico 
+  delayMicroseconds(10);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duracao = pulseIn(echoPin, HIGH);
+  distancia = duracao / 58; //Calculando distancia 
+
+
+void infraRed(){
+   do{
+    servo.write(0);
+  }while(digitalRead(pinSensor) == HIGH);
+}
+
+void levelCheck(){
   if (distancia >= 50 && distancia <= 60) {
     lcd.setCursor(0, 0);
     lcd.print("Cheio");
@@ -131,26 +112,6 @@ void loop() {
     lcd.print("Vazio");
     delay(500);
   }
-
-
-}
-
-void ultrassonico() {
-
-  digitalWrite(trigPin, LOW); //Pulsando o sensor ultrassônico 
-  delayMicroseconds(10);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-
-  duracao = pulseIn(echoPin, HIGH);
-  distancia = duracao / 58; //Calculando distancia 
-
-
-void infraRed(){
-   do{
-    servo.write(0);
-  }while(digitalRead(pinSensor) == HIGH);
 }
 
 
